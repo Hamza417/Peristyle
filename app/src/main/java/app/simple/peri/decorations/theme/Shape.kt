e2635844e199632415ef73083a443c9c18ba9097
Hamza417 <HamzaRizwan243@gmail.com>
2023-12-05 20:58:24 +0530

    v1.16_beta
      - Basic compose theme

---
+++ app/src/main/java/app/simple/peri/decorations/theme/Shape.kt
@@ -0,0 +1,11 @@
+package app.simple.peri.decorations.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(10.dp),
+    large = RoundedCornerShape(16.dp)
+)
---
+++ app/src/main/java/app/simple/peri/decorations/theme/Type.kt
@@ -0,0 +1,18 @@
+package app.simple.peri.decorations.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.5.sp
+    )
+)
