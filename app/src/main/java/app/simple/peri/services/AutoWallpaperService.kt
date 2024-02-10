79ba45be1639ce19369844e14d6092501e4e04a9
Hamza417 <HamzaRizwan243@gmail.com>
2024-02-10 09:43:45 +0530

    v1.30_beta
      - Option to toggle separate wallpaper for lockscreen

--- app/src/main/java/app/simple/peri/services/AutoWallpaperService.kt
+++ app/src/main/java/app/simple/peri/services/AutoWallpaperService.kt
@@ -48,6 +48,7 @@ class AutoWallpaperService : Service() {
     private fun init() {
         SharedPreferences.init(this)
         setWallpaper()
+        setLockScreenWallpaper()
         Log.d("AutoWallpaperService", "Wallpaper set")
     }
 
@@ -92,10 +93,18 @@ class AutoWallpaperService : Service() {
 
                             val visibleCropHint = Rect(left, top, right, bottom)
 
-                            if (MainPreferences.getCropWallpaper()) {
-                                wallpaperManager.setBitmap(bitmap, visibleCropHint, true, WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK)
+                            if (MainPreferences.isDifferentWallpaperForLockScreen()) {
+                                if (MainPreferences.getCropWallpaper()) {
+                                    wallpaperManager.setBitmap(bitmap, visibleCropHint, true, WallpaperManager.FLAG_SYSTEM)
+                                } else {
+                                    wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM)
+                                }
                             } else {
-                                wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK)
+                                if (MainPreferences.getCropWallpaper()) {
+                                    wallpaperManager.setBitmap(bitmap, visibleCropHint, true, WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK)
+                                } else {
+                                    wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK)
+                                }
                             }
 
                             bitmap.recycle()
@@ -111,6 +120,73 @@ class AutoWallpaperService : Service() {
                 Log.d("AutoWallpaperService", "Service stopped, wait for next alarm to start again")
                 withContext(Dispatchers.Main) {
                     stopSelf()
+                }
+            }
+        }
+    }
+
+    private fun setLockScreenWallpaper() {
+        if (MainPreferences.isDifferentWallpaperForLockScreen()) {
+            CoroutineScope(Dispatchers.Default).launch {
+                runCatching {
+                    val dir = DocumentFile.fromTreeUri(applicationContext, Uri.parse(MainPreferences.getStorageUri()))
+                    val files = dir?.listFiles()
+                    files?.random()?.let {
+                        val wallpaperManager = WallpaperManager.getInstance(applicationContext)
+                        it.uri.let { uri ->
+                            contentResolver.openInputStream(uri)?.use { stream ->
+                                val byteArray = stream.readBytes()
+                                val bitmapOptions = BitmapFactory.Options().apply {
+                                    inJustDecodeBounds = true
+                                }
+
+                                BitmapFactory.decodeStream(ByteArrayInputStream(byteArray), null, bitmapOptions)
+
+                                val bitmap = BitmapFactory.decodeStream(ByteArrayInputStream(byteArray), null, BitmapFactory.Options().apply {
+                                    inPreferredConfig = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                        Bitmap.Config.RGBA_1010102
+                                    } else {
+                                        Bitmap.Config.ARGB_8888
+                                    }
+
+                                    Log.d("AutoWallpaperService", "Expected bitmap size: $displayWidth x $displayHeight")
+                                    inSampleSize = BitmapUtils.calculateInSampleSize(bitmapOptions, displayWidth, displayHeight)
+                                    inJustDecodeBounds = false
+                                    Log.d("AutoWallpaperService", "Bitmap decoded with sample size: ${this.inSampleSize}")
+                                })
+
+                                Log.d("AutoWallpaperService", "Bitmap size: ${bitmap?.width}x${bitmap?.height}")
+
+                                val bitmapWidth = bitmap?.width
+                                val bitmapHeight = bitmap?.height
+
+                                val left = (bitmapWidth!! - displayWidth) / 2
+                                val top = 0
+                                val right = left + displayWidth
+                                val bottom = bitmapHeight!!
+
+                                val visibleCropHint = Rect(left, top, right, bottom)
+
+                                if (MainPreferences.getCropWallpaper()) {
+                                    wallpaperManager.setBitmap(bitmap, visibleCropHint, true, WallpaperManager.FLAG_LOCK)
+                                } else {
+                                    wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_LOCK)
+                                }
+
+                                bitmap.recycle()
+
+                                withContext(Dispatchers.Main) {
+                                    stopSelf()
+                                }
+                            }
+                        }
+                    }
+                }.getOrElse {
+                    Log.e("AutoWallpaperService", "Error setting wallpaper: $it")
+                    Log.d("AutoWallpaperService", "Service stopped, wait for next alarm to start again")
+                    withContext(Dispatchers.Main) {
+                        stopSelf()
+                    }
                 }
             }
         }
