4e1daf8e5133759222f7e84f49d7e828a25d0176
Hamza417 <HamzaRizwan243@gmail.com>
2024-09-09 15:45:30 +0530

    Compose home screen rev03
      // Bottom menu design

--- app/src/main/java/app/simple/peri/compose/screens/HomeScreen.kt
+++ app/src/main/java/app/simple/peri/compose/screens/HomeScreen.kt
@@ -2,7 +2,6 @@ package app.simple.peri.compose.screens
 
 import android.content.Context
 import android.graphics.drawable.Drawable
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,7 +9,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
@@ -18,7 +19,10 @@ import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
@@ -34,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -90,7 +95,7 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
                     contentPadding = PaddingValues(horizontal = 32.dp),
                     flingBehavior = fling,
                     modifier = Modifier
-                        .fillMaxSize()
+                        .weight(1f)
             ) { page ->
                 val wallpaper = systemWallpapers.getOrNull(page)
 
@@ -126,6 +131,13 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
 
                 )
             }
+
+            BottomMenu(
+                    context = context,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .height(120.dp)
+            )
         }
     }
 }
@@ -224,5 +236,63 @@ fun Header(title: String, modifier: Modifier = Modifier, navController: NavContr
                     contentDescription = null
             )
         }
+    }
+}
+
+@Composable
+fun BottomMenu(context: Context, modifier: Modifier = Modifier) {
+    Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalAlignment = Alignment.Bottom
+    ) {
+        BottomMenuItem(title = context.getString(R.string.home), Icons.Filled.Home, modifier = Modifier.weight(1f))
+        BottomMenuItem(title = context.getString(R.string.settings),
+                       imageVector = Icons.Filled.Settings,
+                       modifier = Modifier.weight(1f))
+        BottomMenuItem(title = context.getString(R.string.wallpapers), Icons.Filled.AddCircle, modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun BottomMenuItem(title: String = "", imageVector: ImageVector? = null, modifier: Modifier = Modifier) {
+    Column(
+            modifier = modifier
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+                elevation = CardDefaults.cardElevation(
+                        defaultElevation = 0.dp
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(64.dp)
+                    .height(64.dp)
+                    .weight(1f),
+                shape = RoundedCornerShape(32.dp),
+        ) {
+            IconButton(
+                    modifier = Modifier.fillMaxSize(),
+                    onClick = {
+                        // Navigate to the home screen
+                    },
+            ) {
+                Icon(
+                        imageVector = imageVector ?: Icons.Filled.Home,
+                        contentDescription = null,
+                )
+            }
+        }
+
+        Text(
+                text = title,
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp, // Set the font size
+                modifier = Modifier.weight(1f), // Set the weight
+                fontWeight = FontWeight.Bold, // Make the text bold
+        )
     }
 }
