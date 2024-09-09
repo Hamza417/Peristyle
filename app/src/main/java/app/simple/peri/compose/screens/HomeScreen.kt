3bc238d6651a9ba8fe0649ee8b9f8b3dbb491181
Hamza417 <HamzaRizwan243@gmail.com>
2024-09-09 21:43:00 +0530

    Compose wallpaper screen rev01
      // Fixes

--- app/src/main/java/app/simple/peri/compose/screens/HomeScreen.kt
+++ app/src/main/java/app/simple/peri/compose/screens/HomeScreen.kt
@@ -99,7 +99,7 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
 
             HorizontalPager(
                     state = pagerState,
-                    contentPadding = PaddingValues(horizontal = 32.dp),
+                    contentPadding = PaddingValues(horizontal = 48.dp),
                     flingBehavior = fling,
                     modifier = Modifier
                         .weight(1f)
@@ -122,16 +122,26 @@ fun HomeScreen(context: Context, navController: NavController? = null) {
                                 // Calculate the absolute offset for the current page from the
                                 // scroll position. We use the absolute value which allows us to mirror
                                 // any effects for both directions
-                                val pageOffset = (
-                                        (pagerState.currentPage - page) + pagerState
-                                            .currentPageOffsetFraction
-                                        ).absoluteValue
+                                val pageOffset = ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+                                val startScale = 0.95f
 
                                 // We animate the alpha, between 50% and 100%
-                                alpha = lerp(
-                                        start = 0.5f,
+                                // alpha = lerp(
+                                //        start = 0.75f,
+                                //        stop = 1f,
+                                //        fraction = 1f - pageOffset.coerceIn(0f, 1f),
+                                //)
+
+                                scaleX = lerp(
+                                        start = startScale,
                                         stop = 1f,
-                                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                        fraction = 1f - pageOffset.coerceIn(0f, 1f),
+                                )
+
+                                scaleY = lerp(
+                                        start = startScale,
+                                        stop = 1f,
+                                        fraction = 1f - pageOffset.coerceIn(0f, 1f),
                                 )
                             }
                             .padding(8.dp), // Add padding to create space between the cards
@@ -164,8 +174,7 @@ fun CardItem(title: String, onClick: () -> Unit, modifier: Modifier = Modifier, 
                     defaultElevation = 6.dp
             ),
             modifier = modifier
-                .fillMaxHeight()
-                .padding(8.dp), // margin
+                .fillMaxHeight(),
             onClick = onClick,
             shape = RoundedCornerShape(32.dp)
     ) {
