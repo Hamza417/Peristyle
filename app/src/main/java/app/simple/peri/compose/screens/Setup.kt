b984c228c50665f9fd9d0b29b3fc6cc887937853
Hamza417 <HamzaRizwan243@gmail.com>
2024-09-10 16:19:13 +0530

    Fix crashing while storage permission

--- app/src/main/java/app/simple/peri/compose/screens/Setup.kt
+++ app/src/main/java/app/simple/peri/compose/screens/Setup.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -332,8 +333,10 @@ fun RequestDirectoryPermission() {
     }
 
     // Launch the directory selection intent
-    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-    storageResult.launch(intent)
+    LaunchedEffect(Unit) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        storageResult.launch(intent)
+    }
 }
 
 fun isSetupComplete(context: Context): Boolean {
