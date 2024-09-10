b0ae9b83e8c750bd4cd14ff1b4b157bc7ef34b8a
Hamza417 <HamzaRizwan243@gmail.com>
2024-09-10 12:07:31 +0530

    Compose wallpapers screen rev02
     // Add wallpaper menu

--- app/src/main/java/app/simple/peri/compose/screens/Wallpapers.kt
+++ app/src/main/java/app/simple/peri/compose/screens/Wallpapers.kt
@@ -1,35 +1,60 @@
 package app.simple.peri.compose.screens
 
 import android.graphics.drawable.Drawable
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.core.app.ShareCompat
+import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import app.simple.peri.R
 import app.simple.peri.compose.nav.Routes
 import app.simple.peri.models.Wallpaper
 import app.simple.peri.utils.FileUtils.toUri
@@ -45,11 +70,18 @@ import dev.chrisbanes.haze.HazeDefaults
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.haze
 import dev.chrisbanes.haze.hazeChild
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun Wallpapers(navController: NavController? = null) {
     val wallpaperViewModel: WallpaperViewModel = viewModel()
-    val wallpapers = wallpaperViewModel.getWallpapersLiveData().observeAsState(initial = emptyList())
+    var wallpapers by remember { mutableStateOf(emptyList<Wallpaper>()) }
+
+    wallpaperViewModel.getWallpapersLiveData().observeAsState().value?.let {
+        wallpapers = it
+    }
 
     LazyVerticalGrid(
             columns = GridCells.Fixed(2),
@@ -58,16 +90,25 @@ fun Wallpapers(navController: NavController? = null) {
                 .windowInsetsPadding(WindowInsets.safeDrawing)
                 .padding(8.dp)
     ) {
-        items(wallpapers.value.size) { index ->
-            WallpaperItem(wallpapers.value[index], navController)
+        items(wallpapers.size) { index ->
+            WallpaperItem(wallpapers[index], navController) { deletedWallpaper ->
+                wallpapers = wallpapers.filter { it != deletedWallpaper }
+            }
         }
     }
 }
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalFoundationApi::class)
 @Composable
-fun WallpaperItem(wallpaper: Wallpaper, navController: NavController? = null) {
+fun WallpaperItem(wallpaper: Wallpaper, navController: NavController? = null, onDelete: (Wallpaper) -> Unit) {
     val hazeState = remember { HazeState() }
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        WallpaperMenu({
+                          showDialog = it
+                      }, wallpaper, onDelete)
+    }
 
     ElevatedCard(
             elevation = CardDefaults.cardElevation(
@@ -75,12 +116,17 @@ fun WallpaperItem(wallpaper: Wallpaper, navController: NavController? = null) {
             ),
             modifier = Modifier
                 .aspectRatio(9F / 16F)
-                .padding(8.dp),
-            onClick = {
-                navController?.navigate(Routes.WALLPAPER) {
-                    navController.currentBackStackEntry?.savedStateHandle?.set(Routes.WALLPAPER_ARG, wallpaper)
-                }
-            },
+                .padding(8.dp)
+                .combinedClickable(
+                        onClick = {
+                            navController?.navigate(Routes.WALLPAPER) {
+                                navController.currentBackStackEntry?.savedStateHandle?.set(Routes.WALLPAPER_ARG, wallpaper)
+                            }
+                        },
+                        onLongClick = {
+                            showDialog = true
+                        }
+                ),
             shape = RoundedCornerShape(16.dp)
     ) {
         Box(
@@ -130,6 +176,7 @@ fun WallpaperItem(wallpaper: Wallpaper, navController: NavController? = null) {
                         color = Color.White, // Set the text color
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis,
+                        softWrap = false,
                 )
 
                 Text(
@@ -141,6 +188,104 @@ fun WallpaperItem(wallpaper: Wallpaper, navController: NavController? = null) {
                         fontWeight = FontWeight.Light, // Make the text bold
                         color = Color.White, // Set the text color
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun WallpaperMenu(setShowDialog: (Boolean) -> Unit, wallpaper: Wallpaper, onDelete: (Wallpaper) -> Unit, wallpaperViewModel: WallpaperViewModel = viewModel()) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    Dialog(onDismissRequest = { setShowDialog(false) }) {
+        Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = Color.White
+        ) {
+            Box(
+                    contentAlignment = Alignment.Center
+            ) {
+                Column(modifier = Modifier.padding(20.dp)) {
+                    Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                                text = wallpaper.name ?: "",
+                                style = TextStyle(
+                                        fontSize = 24.sp,
+                                        fontFamily = FontFamily.Default,
+                                        fontWeight = FontWeight.Bold
+                                )
+                        )
+                        Icon(
+                                imageVector = Icons.Rounded.Close,
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .width(30.dp)
+                                    .height(30.dp)
+                                    .clickable { setShowDialog(false) }
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Button(
+                            onClick = {
+                                ShareCompat.IntentBuilder(context)
+                                    .setType("image/*")
+                                    .setChooserTitle("Share Wallpaper")
+                                    .setStream(wallpaper.uri.toUri())
+                                    .startChooser()
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.White,
+                            ),
+                            shape = RoundedCornerShape(12.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp),
+                    ) {
+                        Text(
+                                text = context.getString(R.string.send),
+                                color = Color.Black,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.SemiBold
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Button(
+                            onClick = {
+                                coroutineScope.launch(Dispatchers.IO) {
+                                    if (DocumentFile.fromSingleUri(context, wallpaper.uri.toUri())?.delete() == true) {
+                                        withContext(Dispatchers.Main) {
+                                            wallpaperViewModel.removeWallpaper(wallpaper)
+                                            onDelete(wallpaper)
+                                            setShowDialog(false)
+                                        }
+                                    }
+                                }
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.White,
+                            ),
+                            shape = RoundedCornerShape(12.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp),
+                    ) {
+                        Text(
+                                text = context.getString(R.string.delete),
+                                color = Color.Black,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                }
             }
         }
     }
