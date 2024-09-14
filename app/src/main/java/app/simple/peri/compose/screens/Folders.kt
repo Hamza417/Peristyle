f919dc6fa582be49ce7702e62520829557e7ae5b
Hamza417 <HamzaRizwan243@gmail.com>
2024-09-14 20:13:02 +0530

    Add folder image loader

--- app/src/main/java/app/simple/peri/compose/screens/Folders.kt
+++ app/src/main/java/app/simple/peri/compose/screens/Folders.kt
@@ -1,6 +1,5 @@
 package app.simple.peri.compose.screens
 
-import android.graphics.drawable.Drawable
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
@@ -11,9 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -25,7 +24,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,6 +34,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -44,18 +43,14 @@ import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import app.simple.peri.R
 import app.simple.peri.compose.commons.COMMON_PADDING
 import app.simple.peri.compose.commons.TopHeader
 import app.simple.peri.models.Folder
-import app.simple.peri.utils.FileUtils.toUri
 import app.simple.peri.viewmodels.WallpaperViewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import dev.chrisbanes.haze.HazeDefaults
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.haze
@@ -74,6 +69,8 @@ fun Folders(navController: NavController? = null) {
     navigationBarHeight = WindowInsetsCompat.toWindowInsetsCompat(
             LocalView.current.rootWindowInsets).getInsets(WindowInsetsCompat.Type.navigationBars()).bottom
     count = LocalContext.current.contentResolver.persistedUriPermissions.size
+    displayDimension.width = LocalView.current.width
+    displayDimension.height = LocalView.current.height
 
     wallpaperViewModel.getFoldersLiveData().observeAsState().value?.let {
         folders = it
@@ -87,8 +84,8 @@ fun Folders(navController: NavController? = null) {
     val topPadding = 8.dp + statusBarHeightDp
     val bottomPadding = 8.dp + navigationBarHeightDp
 
-    LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
+    LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(2),
             modifier = Modifier
                 .fillMaxSize(),
             contentPadding = PaddingValues(
@@ -97,8 +94,8 @@ fun Folders(navController: NavController? = null) {
                     end = 8.dp,
                     bottom = bottomPadding)
     ) {
-        item(span = { GridItemSpan(maxLineSpan) }) {
-            TopHeader(title = "Wallpapers", count = count,
+        item(span = StaggeredGridItemSpan.FullLine) {
+            TopHeader(title = stringResource(R.string.folder), count = count,
                       modifier = Modifier.padding(COMMON_PADDING),
                       navController = navController)
         }
@@ -111,29 +108,22 @@ fun Folders(navController: NavController? = null) {
             ElevatedCard(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(COMMON_PADDING)
-                        .aspectRatio(1f),
+                        .padding(8.dp)
+                        .aspectRatio(displayDimension.getAspectRatio()),
                     shape = RoundedCornerShape(16.dp),
                     elevation = CardDefaults.cardElevation(
-                            defaultElevation = 16.dp,
+                            defaultElevation = 8.dp,
                     ),
                     onClick = {
                         // TODO add folder
                     }
             ) {
-                Box(
-                        modifier = Modifier
-                            .padding(COMMON_PADDING)
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                ) {
-                    Icon(
-                            imageVector = Icons.Rounded.Add,
-                            contentDescription = "Add folder",
-                            modifier = Modifier.fillMaxSize(),
-                            tint = MaterialTheme.colorScheme.surfaceDim
-                    )
-                }
+                Icon(
+                        imageVector = Icons.Rounded.Add,
+                        contentDescription = "Add folder",
+                        modifier = Modifier.fillMaxSize(),
+                        tint = MaterialTheme.colorScheme.surfaceDim
+                )
             }
         }
     }
@@ -144,91 +134,57 @@ fun Folders(navController: NavController? = null) {
 fun FolderItem(folder: Folder, navController: NavController? = null, onDelete: (Folder) -> Unit) {
     val hazeState = remember { HazeState() }
 
-    var displayWidth by remember { mutableIntStateOf(0) }
-    var displayHeight by remember { mutableIntStateOf(0) }
+    ElevatedCard(
+            elevation = CardDefaults.cardElevation(
+                    defaultElevation = 16.dp,
+            ),
+            modifier = Modifier
+                .padding(8.dp)
+                .combinedClickable(
+                        onClick = {
 
-    displayWidth = LocalView.current.width
-    displayHeight = LocalView.current.height
+                        },
+                        onLongClick = {
 
-    val aspectRatio by remember {
-        mutableFloatStateOf(displayWidth.toFloat() / displayHeight.toFloat())
-    }
-
-    Box {
-        ElevatedCard(
-                elevation = CardDefaults.cardElevation(
-                        defaultElevation = 16.dp,
+                        }
                 ),
-                modifier = Modifier
-                    .aspectRatio(aspectRatio)
-                    .padding(COMMON_PADDING)
-                    .combinedClickable(
-                            onClick = {
-
-                            },
-                            onLongClick = {
-
-                            }
-                    ),
-                shape = RoundedCornerShape(16.dp),
+            shape = RoundedCornerShape(16.dp),
+    ) {
+        Box(
+                modifier = Modifier.wrapContentHeight(),
+                contentAlignment = Alignment.Center,
         ) {
-            Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center,
+            GlideImage(
+                    model = app.simple.peri.glide.folders.Folder(folder.hashcode, context = LocalContext.current),
+                    contentDescription = null,
+                    modifier = Modifier.haze(hazeState),
             ) {
-                GlideImage(
-                        model = folder.uri.toUri(),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .haze(hazeState),
-                        alignment = Alignment.Center,
-                ) {
-                    it.addListener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(
-                                e: GlideException?,
-                                model: Any?,
-                                target: Target<Drawable>,
-                                isFirstResource: Boolean): Boolean {
-                            return false
-                        }
+                it
+                    .transition(withCrossFade())
+                    .disallowHardwareConfig()
+            }
 
-                        override fun onResourceReady(
-                                resource: Drawable,
-                                model: Any,
-                                target: Target<Drawable>?,
-                                dataSource: DataSource,
-                                isFirstResource: Boolean): Boolean {
-                            return false
-                        }
-                    })
-                        .transition(withCrossFade())
-                        .disallowHardwareConfig()
-                        .centerCrop()
-                }
-
-                Column(
+            Column(
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .fillMaxWidth()
+                        .hazeChild(
+                                state = hazeState,
+                                style = HazeDefaults.style(backgroundColor = Color(0x50000000), blurRadius = 5.dp))
+                        .align(Alignment.BottomCenter)
+            ) {
+                Text(
+                        text = folder.name ?: "",
                         modifier = Modifier
-                            .wrapContentHeight()
-                            .fillMaxWidth()
-                            .hazeChild(
-                                    state = hazeState,
-                                    style = HazeDefaults.style(backgroundColor = Color(0x50000000), blurRadius = 5.dp))
-                            .align(Alignment.BottomCenter)
-                ) {
-                    Text(
-                            text = folder.name ?: "",
-                            modifier = Modifier
-                                .padding(16.dp),
-                            textAlign = TextAlign.Start,
-                            fontSize = 18.sp, // Set the font size
-                            fontWeight = FontWeight.Bold, // Make the text bold
-                            color = Color.White, // Set the text color
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            softWrap = false,
-                    )
-                }
+                            .padding(16.dp),
+                        textAlign = TextAlign.Start,
+                        fontSize = 18.sp, // Set the font size
+                        fontWeight = FontWeight.Bold, // Make the text bold
+                        color = Color.White, // Set the text color
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        softWrap = false,
+                )
             }
         }
     }
