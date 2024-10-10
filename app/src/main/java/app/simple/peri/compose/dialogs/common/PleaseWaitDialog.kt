245185d4e73787b4e2bbe773c07fdc07b6a1b8d7
Hamza417 <HamzaRizwan243@gmail.com>
2024-10-10 11:31:47 +0530

    Optimize please wait dialog

--- app/src/main/java/app/simple/peri/compose/dialogs/common/PleaseWaitDialog.kt
+++ app/src/main/java/app/simple/peri/compose/dialogs/common/PleaseWaitDialog.kt
@@ -2,6 +2,7 @@ package app.simple.peri.compose.dialogs.common
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import app.simple.peri.R
 
 @Composable
@@ -20,6 +22,7 @@ fun PleaseWaitDialog(onDismissRequest: () -> Unit) {
         title = { },
         text = {
             Row(
+                modifier = Modifier.padding(top = 8.dp),
                 verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
             ) {
                 CircularProgressIndicator(
@@ -27,7 +30,7 @@ fun PleaseWaitDialog(onDismissRequest: () -> Unit) {
                     color = MaterialTheme.colorScheme.primary
                 )
                 Spacer(modifier = Modifier.width(16.dp))
-                Text(stringResource(id = R.string.preparing))
+                Text(stringResource(id = R.string.please_wait))
             }
         },
         confirmButton = {
@@ -35,6 +38,10 @@ fun PleaseWaitDialog(onDismissRequest: () -> Unit) {
         },
         dismissButton = {
 
-        }
+        },
+        properties = DialogProperties(
+            dismissOnBackPress = false,
+            dismissOnClickOutside = false
+        )
     )
 }
