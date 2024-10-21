55c5362f3095e831aaae07a0e529545b28dbef47
Hamza417 <HamzaRizwan243@gmail.com>
2024-10-21 16:41:27 +0530

    Add a random wallpaper page on Home screen

--- app/src/main/java/app/simple/peri/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/app/simple/peri/viewmodels/HomeScreenViewModel.kt
@@ -5,12 +5,15 @@ import android.app.WallpaperManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import app.simple.peri.database.instances.WallpaperDatabase
 import app.simple.peri.models.Wallpaper
 import app.simple.peri.utils.PermissionUtils
 import kotlinx.coroutines.Dispatchers
@@ -18,6 +21,8 @@ import kotlinx.coroutines.launch
 import java.io.File
 
 class HomeScreenViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val handler = Handler(Looper.getMainLooper())
 
     private val systemWallpaperData: MutableLiveData<ArrayList<Wallpaper>> by lazy {
         MutableLiveData<ArrayList<Wallpaper>>().also {
@@ -36,6 +41,8 @@ class HomeScreenViewModel(application: Application) : AndroidViewModel(applicati
                 systemWallpaperData.postValue(getCurrentSystemWallpaper())
             }
         }
+
+        handler.postDelayed(randomWallpaperRepeatRunnable, 10000)
     }
 
     private fun getCurrentSystemWallpaper(): ArrayList<Wallpaper> {
@@ -62,22 +69,51 @@ class HomeScreenViewModel(application: Application) : AndroidViewModel(applicati
         val lockUri = getFileUri(lockFile)
 
         return arrayListOf(
-                Wallpaper().createFromUri(systemUri.toString(), getApplication()),
-                Wallpaper().createFromUri(lockUri.toString(), getApplication())
+            Wallpaper().createFromUri(systemUri.toString(), getApplication()),
+            Wallpaper().createFromUri(lockUri.toString(), getApplication()),
+            getRandomWallpaperFromDatabase()
         )
     }
 
     private fun createTempFile(fileName: String): File {
-        val file = File(getApplication<Application>().filesDir,
-                        fileName.replace("$", System.currentTimeMillis().div(1000).toString()))
+        val file = File(
+            getApplication<Application>().filesDir,
+            fileName.replace("$", System.currentTimeMillis().div(1000).toString())
+        )
         if (file.exists()) file.delete()
         return file
     }
 
     private fun getFileUri(file: File): Uri {
         return FileProvider.getUriForFile(
-                getApplication(), "${getApplication<Application>().packageName}.provider", file
+            getApplication(), "${getApplication<Application>().packageName}.provider", file
         )
+    }
+
+    private fun getRandomWallpaperFromDatabase(): Wallpaper {
+        val wallpaperDatabase = WallpaperDatabase.getInstance(getApplication())
+        val wallpaper = wallpaperDatabase?.wallpaperDao()?.getRandomWallpaper()
+        return wallpaper ?: Wallpaper()
+    }
+
+    private val randomWallpaperRepeatRunnable = object : Runnable {
+        override fun run() {
+            viewModelScope.launch(Dispatchers.Default) {
+                systemWallpaperData.postValue(systemWallpaperData.value?.apply {
+                    this[2] = getRandomWallpaperFromDatabase()
+                })
+
+                Log.i("HomeScreenViewModel", "Posting random wallpaper")
+            }
+
+            handler.removeCallbacksAndMessages(null)
+            handler.postDelayed(this, 10000)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        handler.removeCallbacksAndMessages(null)
     }
 
     companion object {
