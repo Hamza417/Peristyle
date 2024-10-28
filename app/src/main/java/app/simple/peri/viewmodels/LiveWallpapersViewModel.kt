62241b53cf2142142551ac2466986cf5d26f8ce1
Hamza417 <HamzaRizwan243@gmail.com>
2024-10-28 23:45:00 +0530

    Update live wallpapers list after list state changes

--- app/src/main/java/app/simple/peri/viewmodels/LiveWallpapersViewModel.kt
+++ app/src/main/java/app/simple/peri/viewmodels/LiveWallpapersViewModel.kt
@@ -2,10 +2,14 @@ package app.simple.peri.viewmodels
 
 import android.app.Application
 import android.app.WallpaperInfo
+import android.content.Context
 import android.content.Intent
+import android.content.pm.LauncherApps
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
+import android.os.UserHandle
 import android.service.wallpaper.WallpaperService
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -14,6 +18,40 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class LiveWallpapersViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val launcherAppsService: LauncherApps by lazy {
+        getApplication<Application>().getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+    }
+
+    private var launcherAppsCallback: LauncherApps.Callback? = null
+
+    init {
+        if (launcherAppsCallback == null) {
+            launcherAppsCallback = object : LauncherApps.Callback() {
+                override fun onPackageRemoved(packageName: String, user: UserHandle) {
+                    getLiveWallpapers()
+                }
+
+                override fun onPackageAdded(packageName: String, user: UserHandle) {
+                    getLiveWallpapers()
+                }
+
+                override fun onPackageChanged(packageName: String, user: UserHandle) {
+                    getLiveWallpapers()
+                }
+
+                override fun onPackagesAvailable(packageNames: Array<out String>?, user: UserHandle?, replacing: Boolean) {
+                    getLiveWallpapers()
+                }
+
+                override fun onPackagesUnavailable(packageNames: Array<out String>?, user: UserHandle?, replacing: Boolean) {
+                    getLiveWallpapers()
+                }
+            }
+
+            launcherAppsService.registerCallback(launcherAppsCallback!!)
+        }
+    }
 
     private val liveWallpapers: MutableLiveData<List<LiveWallpaperInfo>> by lazy {
         MutableLiveData<List<LiveWallpaperInfo>>().also {
@@ -25,6 +63,7 @@ class LiveWallpapersViewModel(application: Application) : AndroidViewModel(appli
 
     private fun getLiveWallpapers() {
         viewModelScope.launch(Dispatchers.Default) {
+            Log.i("LiveWallpapersViewModel", "getLiveWallpapers")
             val packageManager: PackageManager = getApplication<Application>().packageManager
             val availableWallpapersList: MutableList<ResolveInfo> = packageManager.queryIntentServices(
                     Intent(WallpaperService.SERVICE_INTERFACE), PackageManager.GET_META_DATA)
@@ -44,5 +83,10 @@ class LiveWallpapersViewModel(application: Application) : AndroidViewModel(appli
 
             liveWallpapers.postValue(wallpapers.sortedBy { it.name })
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        launcherAppsService.unregisterCallback(launcherAppsCallback!!)
     }
 }
