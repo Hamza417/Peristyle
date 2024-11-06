6631d76c7ae53a37e23dcd6c14daa5168d87250f
Hamza417 <HamzaRizwan243@gmail.com>
2024-11-07 01:11:28 +0530

    Add settings activity

---
+++ app/src/main/java/app/simple/peri/activities/main/ComposePreferencesActivity.kt
@@ -0,0 +1,29 @@
+package app.simple.peri.activities.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import app.simple.peri.compose.screens.Settings
+import app.simple.peri.compose.theme.PeristyleTheme
+import app.simple.peri.preferences.SharedPreferences
+
+class ComposePreferencesActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SharedPreferences.init(applicationContext)
+
+        setContent {
+            PeristyleTheme {
+                Surface(
+                        modifier = Modifier.fillMaxSize()
+                ) {
+                    Settings()
+                }
+            }
+        }
+    }
+}
