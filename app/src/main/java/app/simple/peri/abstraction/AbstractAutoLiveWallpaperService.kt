eadb2ae914bba36412e3dfd8cec890680e8ad5b2
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-01-18 12:35:43 +0530

    add: live wallpaper basic communication impl #122

--- app/src/main/java/app/simple/peri/abstraction/AbstractAutoLiveWallpaperService.kt
+++ app/src/main/java/app/simple/peri/abstraction/AbstractAutoLiveWallpaperService.kt
@@ -2,6 +2,7 @@ package app.simple.peri.abstraction
 
 import android.app.ActivityManager
 import android.content.Context
+import android.os.Parcelable
 import android.service.wallpaper.WallpaperService
 import app.simple.peri.models.Wallpaper
 import app.simple.peri.services.LiveAutoWallpaperService
@@ -15,20 +16,34 @@ abstract class AbstractAutoLiveWallpaperService : AbstractComposeAutoWallpaperSe
     }
 
     override fun setSameWallpaper(wallpaper: Wallpaper) {
-
+        val intent = LiveAutoWallpaperService.getIntent(applicationContext, LiveAutoWallpaperService.SAME_WALLPAPER)
+        intent.putExtra(LiveAutoWallpaperService.EXTRA_WALLPAER, wallpaper as Parcelable)
+        applicationContext.startService(intent)
     }
 
     override fun setHomeScreenWallpaper(wallpaper: Wallpaper) {
-
+        val intent = LiveAutoWallpaperService.getIntent(applicationContext, LiveAutoWallpaperService.HOME_SCREEN_WALLPAPER)
+        intent.putExtra(LiveAutoWallpaperService.EXTRA_WALLPAER, wallpaper as Parcelable)
+        applicationContext.startService(intent)
     }
 
     override fun setLockScreenWallpaper(wallpaper: Wallpaper) {
-
+        val intent = LiveAutoWallpaperService.getIntent(applicationContext, LiveAutoWallpaperService.LOCK_SCREEN_WALLPAPER)
+        intent.putExtra(LiveAutoWallpaperService.EXTRA_WALLPAER, wallpaper as Parcelable)
+        applicationContext.startService(intent)
     }
 
     @Suppress("DEPRECATION")
     protected fun isWallpaperServiceRunning(): Boolean {
         val activityManager = applicationContext.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        /**
+         * "As of Build. VERSION_CODES. O, this method is no longer available
+         * to third party applications. For backwards compatibility, it will still
+         * return the caller's own services."
+         *
+         * If we are querying my app's services, this method will work just fine.
+         */
         val services = activityManager.getRunningServices(Int.MAX_VALUE)
         for (service in services) {
             if (LiveAutoWallpaperService::class.java.name == service.service.className) {
@@ -42,6 +57,14 @@ abstract class AbstractAutoLiveWallpaperService : AbstractComposeAutoWallpaperSe
     @Suppress("DEPRECATION")
     private fun isWallpaperServiceRunning(context: Context, serviceClass: Class<out WallpaperService>): Boolean {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        /**
+         * "As of Build. VERSION_CODES. O, this method is no longer available
+         * to third party applications. For backwards compatibility, it will still
+         * return the caller's own services."
+         *
+         * If we are querying my app's services, this method will work just fine.
+         */
         val services = activityManager.getRunningServices(Int.MAX_VALUE)
         for (service in services) {
             if (serviceClass.name == service.service.className) {
--- app/src/main/java/app/simple/peri/services/LiveAutoWallpaperService.kt
+++ app/src/main/java/app/simple/peri/services/LiveAutoWallpaperService.kt
@@ -1,7 +1,9 @@
 package app.simple.peri.services
 
+import android.content.Context
 import android.content.Intent
 import android.service.wallpaper.WallpaperService
+import android.util.Log
 import android.view.SurfaceHolder
 
 class LiveAutoWallpaperService : WallpaperService() {
@@ -11,6 +13,18 @@ class LiveAutoWallpaperService : WallpaperService() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            SAME_WALLPAPER -> {
+                Log.i("LiveAutoWallpaperService", "Setting the same wallpaper")
+            }
+            HOME_SCREEN_WALLPAPER -> {
+                Log.i("LiveAutoWallpaperService", "Setting the wallpaper for the home screen")
+            }
+            LOCK_SCREEN_WALLPAPER -> {
+                Log.i("LiveAutoWallpaperService", "Setting the wallpaper for the lock screen")
+            }
+        }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
@@ -42,6 +56,19 @@ class LiveAutoWallpaperService : WallpaperService() {
 
         override fun onSurfaceDestroyed(holder: SurfaceHolder?) {
             super.onSurfaceDestroyed(holder)
+        }
+    }
+
+    companion object {
+        const val SAME_WALLPAPER = "action.SAME_WALLPAPER"
+        const val HOME_SCREEN_WALLPAPER = "action.HOME_SCREEN_WALLPAPER"
+        const val LOCK_SCREEN_WALLPAPER = "action.LOCK_SCREEN_WALLPAPER"
+        const val EXTRA_WALLPAER = "extra.WALLPAPER"
+
+        fun getIntent(context: Context, action: String): Intent {
+            return Intent(context, LiveAutoWallpaperService::class.java).apply {
+                this.action = action
+            }
         }
     }
 }
