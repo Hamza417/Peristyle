49de246550827011b8c462d8f4ceb2af271b90ff
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-07-16 14:44:30 +0530

    remove: api key prefs
    // Purity 100 does not need API key for WallHaven

--- app/src/main/java/app/simple/peri/models/WallhavenFilter.kt
+++ app/src/main/java/app/simple/peri/models/WallhavenFilter.kt
@@ -3,7 +3,7 @@ package app.simple.peri.models
 data class WallhavenFilter(
         val query: String = "",
         val categories: String = "111",
-        val purity: String = "110",
+        val purity: String = "100",
         val atleast: String = "1920x1080",
         val resolution: String = "1920x1080",
         val ratios: String = "16x9",
