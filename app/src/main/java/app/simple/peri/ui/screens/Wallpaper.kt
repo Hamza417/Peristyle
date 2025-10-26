5b32d6da5779a4f0ffa21e7d06fb31694f78e61c
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-10-26 07:08:35 +0530

    replace: deprecated hilt

--- app/src/main/java/app/simple/peri/ui/screens/Wallpaper.kt
+++ app/src/main/java/app/simple/peri/ui/screens/Wallpaper.kt
@@ -56,7 +56,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import app.simple.peri.R
